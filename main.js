07923da8f79750ef270248f28b4ed4b8ee79161e
Marta SH <msastreharo@gmail.com>
2021-04-30 15:32:48 +0200

    Modified function showRandomName(), splice now works correctly.

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-let names = ["Alisa", "Amanda", "Ana García", "Ana Lissette", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmín"];
+const names = ["Alisa", "Amanda", "Ana García", "Ana Lissette", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmín"];
+let remainingNames = names;
 
 let spinner = document.getElementById("spin");
 
-function randomize() {
-    let randomName = names[Math.floor(Math.random()*names.length)];
+function showRandomName() {
+    let randomName = remainingNames[Math.floor(Math.random()*remainingNames.length)];
 
     let chosenPerson = document.getElementById("chosenName");
     chosenPerson.innerHTML = randomName;
 
-    let deletedPerson = names.indexOf(randomName);
-    let deleted = names.splice(deletedPerson, 1);
+    let deletedPerson = remainingNames.indexOf(randomName);
+    let deleted = remainingNames.splice(deletedPerson, 1);
+
+    if (remainingNames.length === 0) {
+        remainingNames = names;
+    }
+
 
 }
 
-
-
-spinner.addEventListener("click", randomize);
+spinner.addEventListener("click", showRandomName);
 
