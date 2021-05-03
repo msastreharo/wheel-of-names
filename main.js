814ed453b5b3abc5d7babaa48a22ccb8964946e7
Marta SH <msastreharo@gmail.com>
2021-05-03 15:36:33 +0200

    Restart function done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const names = ["Alisa", "Amanda", "Ana García", "Ana Lissette", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmín"];
+let names = ["Alisa", "Amanda", "Ana García", "Ana Lissette", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmín"];
 let namesAlreadyPicked = [];
 
 const spinnerBtn = document.getElementById("spin");
@@ -11,27 +11,25 @@ function showRandomName() {
     let randomName = names[Math.floor(Math.random()*names.length)];
     namePickedDiv.innerHTML = randomName;
     namesAlreadyPicked.push(randomName);
-    displayChosen(randomName);
+    displayChosen();
     deleteChosenFromArray(randomName);
+    if (names.length === 0) {
+        restart();
+    }
 }
 
-function displayChosen(randomName) {
-   listOfNames.innerText = namesAlreadyPicked;
+function displayChosen() {
+   listOfNames.innerText = namesAlreadyPicked.join(", ");
 }
 
 function deleteChosenFromArray(randomName) {
-    console.log(names);
-    //console.log(names.indexOf(randomName));
-    if (namesAlreadyPicked[i] == names[randomName]) {
-        names.splice(names.indexOf(randomName), 1);
-    }
-
+    names.splice(names.indexOf(randomName), 1);
 }
 
-/* let deletedPerson = remainingNames.indexOf(randomName);
-let deleted = remainingNames.splice(deletedPerson, 1);
-
-if (remainingNames.length === 0) {
-    remainingNames = names;
+function restart() {
+    names = [...namesAlreadyPicked];
+    // console.log(names === namesAlreadyPicked);
+    namesAlreadyPicked = [];
 }
-*/
+
+
